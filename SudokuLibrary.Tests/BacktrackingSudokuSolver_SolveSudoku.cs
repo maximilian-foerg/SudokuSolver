d72544d3f56a33d0cb2acdb32ee3432d199978f6
Maximilian Förg <maximilian.foerg@mailbox.org>
2021-04-30 17:24:19 +0200

    Repaired tests

--- SudokuLibrary.Tests/BacktrackingSudokuSolver_SolveSudoku.cs
+++ SudokuLibrary.Tests/BacktrackingSudokuSolver_SolveSudoku.cs
@@ -6,22 +6,21 @@ namespace SudokuLibrary.Tests
     public class BacktrackingSudokuSolver_SolveSudoku
     {
         [Fact]
-        public void SolveSudoku_InputIsSolvable_ReturnSolution()
+        public async void SolveSudoku_InputIsSolvable_ReturnSolution()
         {
             Sudoku sudoku = SudokuParser.FromString("600408000403900700090000503009006000002004380000003627940070030070080090310000206");
-            Sudoku solution = SudokuParser.FromString("657438912423915768891267543739826154162754389584193627945672831276381495318549276");
             BacktrackingSudokuSolver solver = new();
-            Sudoku assignment = solver.SolveSudoku(sudoku);
-            Assert.Equal(assignment, solution);
+            await solver.SolveSudokuAsync(sudoku);
+            Assert.True(sudoku.IsSolved());
         }
 
         [Fact]
-        public void SolveSudoku_InputIsUnsolvable_ReturnSolution()
+        public async void SolveSudoku_InputIsUnsolvable_ReturnSolution()
         {
             Sudoku sudoku = SudokuParser.FromString("600408000403900700090000503009006000002044380000003627940070030070080090310000206");
             BacktrackingSudokuSolver solver = new();
-            Sudoku assignment = solver.SolveSudoku(sudoku);
-            Assert.False(assignment.IsSolved(), "This sudoku should be unsolvable...");
+            await solver.SolveSudokuAsync(sudoku);
+            Assert.False(sudoku.IsSolved());
         }
     }
 }
